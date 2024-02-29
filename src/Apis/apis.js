5ca7bd0dd4c0acf14c931ec3ceff4101a31d5784
Avinash Chandra <77578905+avinashchandra043@users.noreply.github.com>
2024-02-29 17:47:10 +0530

    Update apis.js

--- src/Apis/apis.js
+++ src/Apis/apis.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+
+
+export const API_BASE_URL = "https://crazycart.onrender.com";
 
 export const baseApi = axios.create({
   baseURL: API_BASE_URL,
