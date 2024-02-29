dd19efdec8dd06abbc5c52ab169ea92d5cb401f1
Avinash <avinashchandrabarik@gmail.com>
2024-02-29 19:01:40 +0530

    second Phase

--- src/Apis/apis.js
+++ src/Apis/apis.js
@@ -1,11 +1,39 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+const PROD = "PROD";
+const DEV = "DEV";
+const currentEnvironment = PROD;
+
+const Api = {
+  PROD: {
+    eCommerceApi: "https://crazycart.onrender.com",
+  },
+  DEV: {
+    eCommerceApi: "http://localhost:5454",
+  },
+};
+
+const getApiUrls = (enviroment = currentEnvironment) => {
+  switch (enviroment) {
+    case DEV:
+      return Api.DEV;
+    case PROD:
+      return Api.PROD;
+    default:
+      return Api.DEV;
+  }
+};
+
+export const API = getApiUrls(currentEnvironment);
 
 export const baseApi = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API.eCommerceApi,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("jwt")}`,
     "Content-Type": "application/json",
   },
+});
+
+export const eCommerceApi = axios.create({
+  baseURL: API.eCommerceApi,
 });
